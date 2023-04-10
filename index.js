a4fea815acf0581f1eb4a494c46ce7d515ee0185
THINESHKUMAR <thinesh12@gmal.com>
2023-04-09 21:48:24 -0400

    edit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+const myName = "THINESH";
+
+newHeader.textContent = `${myName} is the champion`;
+document.body.append(newHeader);
